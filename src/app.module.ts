e9a653f4e738e5151cf83781b26de9ac50da477b
Victor Baggio <baggiobaggiovictor@gmail.com>
2024-10-07 18:51:09 -0300

    feat: Adicionado configuração do banco de dados e ambiente de desenvolvimento

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseConfig } from './config/database';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    DatabaseConfig,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
