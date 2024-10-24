9674452189a72212d0912d2475687412e10b8b4c
Victor Baggio <65255655+baggiovictor@users.noreply.github.com>
2024-10-24 17:27:38 -0300

    refactor: Removido modulo de partidas (#24)

--- src/core/match/application/use-cases/create-match.use-case.ts
+++
@@ -1,5 +0,0 @@
-import { CreateMatchCommand } from '../commands/create-match.command';
-
-export interface CreateMatchUseCase {
-  execute(command: CreateMatchCommand): Promise<string>;
-}
--- src/core/match/application/use-cases/edit-match.use-case.ts
+++
@@ -1,5 +0,0 @@
-import { EditMatchCommand } from '../commands/edit-match.command';
-
-export interface EditMatchUseCase {
-  execute(id: string, command: EditMatchCommand): Promise<void>;
-}
