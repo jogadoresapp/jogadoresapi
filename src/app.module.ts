1e03187ef2174c7571f86c976d4e96a7cd71b1b9
Victor Baggio <baggiobaggiovictor@gmail.com>
2024-10-24 17:33:50 -0300

    fix: Ajuste no app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { DatabaseConfig } from './config/database';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './core/auth/auth.module';
 import { PlayerModule } from './core/player/player.module';
-import { MatchModule } from './core/match/match.module';
 
 @Module({
   imports: [
@@ -14,7 +13,6 @@ import { MatchModule } from './core/match/match.module';
     DatabaseConfig,
     PlayerModule,
     AuthModule,
-    MatchModule,
   ],
 })
 export class AppModule {}
